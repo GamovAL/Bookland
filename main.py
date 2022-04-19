afae4f0eb5a7426af9554858a916675c30d887b4
Ilya <Khaknazarov04@mail.ru>
2022-04-19 20:14:37 +0300

    Доделал корзину

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request, session, send_from_directory
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, current_user
 import json
 
 from data import db_session
@@ -7,6 +7,9 @@ from data.users import User
 from data.book import Book
 from forms.login_form import LoginForm
 from forms.register import RegisterForm
+from data.order import Order, Association
+from datetime import datetime, timedelta
+from forms.order_form import Order_Form
 
 
 app = Flask(__name__)
@@ -112,6 +115,58 @@ def basket():                                   # Корзина
         if not basket:
             order_button = False
         return render_template('basket.html', books=books, sum=summa, order_button=order_button)
+
+
+@app.route('/prepend/<id>')
+def prepend(id):
+    basket = json.loads(session['basket'])
+    basket[id] -= 1
+    if basket[id] < 1:
+        basket[id] = 1
+    session['basket'] = json.dumps(basket)
+    return redirect('/basket')
+
+
+@app.route('/append/<id>')
+def append(id):
+    basket = json.loads(session['basket'])
+    basket[id] += 1
+    session['basket'] = json.dumps(basket)
+    return redirect('/basket')
+
+
+@app.route('/del/<id>')
+def basket_delet(id):
+    basket = json.loads(session['basket'])
+    del basket[id]
+    session['basket'] = json.dumps(basket)
+    return redirect('/basket')
+
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    order_form = Order_Form(time_delivery=datetime.now() + timedelta(hours=1))
+    if current_user.is_authenticated:
+        order_form.user_name.data = current_user.name
+        order_form.address.data = current_user.address
+        order_form.telephone.data = current_user.telephone
+    basket = json.loads(session['basket'])
+    if order_form.validate_on_submit():
+        db_sess = db_session.create_session()
+        order = Order(
+            user_name=order_form.user_name.data,
+            telephone=order_form.telephone.data,
+            address=order_form.address.data,
+            time_delivery=order_form.time_delivery.data)
+        for book in basket:
+            a = Association(count=basket[book])
+            a.book = db_sess.query(Book).get(int(book))
+            order.books.append(a)
+        session['basket'] = json.dumps({})
+        db_sess.add(order)
+        db_sess.commit()
+        return render_template('order_ok.html', title='Заказ принят')
+    return render_template('order.html', title='Оформление заказа', form=order_form)
 
 
 @app.template_filter('strftime')
